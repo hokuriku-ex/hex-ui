560ef1a8817421f4507679d8cf6c7df96fdb21d0
hokuriku-ex <design-room@hokuriku-ex.co.jp>
2026-07-16 04:40:23 +0000

    2026年7月16日 13:40

--- dev/js/hex.js
+++ dev/js/hex.js
@@ -250,7 +250,7 @@ function hexInitAnchorNav(){
     return getHexAnchorHeaderHeight()+nav.offsetHeight+40;
   }
   function refreshHexAnchorNav(){
-    var mobileAdjust=window.innerWidth<=768?8:0;
+    var mobileAdjust=window.innerWidth<=768?10:0;
     nav.classList.remove('is-fixed');
     placeholder.classList.remove('is-active');
     placeholder.style.height='0px';
